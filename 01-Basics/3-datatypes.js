981faf2306cab6e605f3c6f1e340053a189c0c81
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-09 18:48:17 +0000

    Datatypes , null as object,

---
+++ 01-Basics/3-datatypes.js
@@ -0,0 +1,17 @@
+
+const patientId = 12345
+let patientName = "Usama"
+var password = "wert"
+
+city = "Islamabad"
+
+
+// patientName = "Ali"
+// password = "sert"
+// city = "Rawalpindi"
+
+console.table([patientId, patientName, password, city])
+console.log(typeof 12345)
+console.log(typeof "Usama")
+console.log(typeof null)
+console.log(typeof undefined)
