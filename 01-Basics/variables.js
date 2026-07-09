f4f50fdb35982032b7123e44683e0ddfa7354dce
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-09 14:30:26 +0000

    const, var, let in JS

---
+++ 01-Basics/variables.js
@@ -0,0 +1,12 @@
+const patientId = 12345
+let patientName = "Usama"
+var password = "wert"
+
+city = "Islamabad"
+
+
+// patientName = "Ali"
+// password = "sert"
+// city = "Rawalpindi"
+
+console.table([patientId, patientName, password, city])
