0cce38ecafd0b31f9555cb14bd05fd2f2b9be0f4
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-10 18:19:29 +0000

    String operations

---
+++ 01-Basics/05-string.js
@@ -0,0 +1,20 @@
+let firstname = "Sana"
+let secondname = "Ullah"
+
+console.log(`My first name is ${firstname} and my second name is ${secondname}`);
+
+const information = new String('sana ullah')
+
+console.log(information.length);
+console.log(information[0])
+console.log(information[1])
+console.log(information[2])
+console.log(information[3])
+console.log(information[4])
+console.log(information[5])
+console.log(information[6])
+console.log(information[7])
+console.log(information[8])
+console.log(information[9])
+
+console.log(information.toLocaleUpperCase());
