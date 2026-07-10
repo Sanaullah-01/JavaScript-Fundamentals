edb51f1bc194799bc6d1ae756980bb36afcad5fa
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-10 09:10:13 +0000

    datatype conversion

---
+++ 01-Basics/03-datatypecon.js
@@ -0,0 +1,22 @@
+// let score = undefined
+
+// console.log (typeof score)
+// console.log (typeof (score))
+
+// console.log (score)
+
+// let valueNum = Number(score)
+
+// console.log (typeof valueNum)
+
+// console.log (valueNum)
+
+// "33" => 33 number
+// "123ert" => number but NAN
+// True => 1,,  false => 0
+
+let isBoolean = 0
+
+let isBooleanvalue = Boolean (isBoolean)
+
+console.log(isBooleanvalue)
