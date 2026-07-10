4eba02fd378b2a39fa600777f017564198debd23
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-10 17:05:47 +0000

    Refrence datatype, array, objects, functions

---
+++ 01-Basics/04-array.js
@@ -0,0 +1,17 @@
+const singer = ["kendric lamar", "Seza", "Doga Cat"]
+
+let myobj = {
+    name:"Sana Ullah",
+    age:"23",
+
+}
+
+console.table(singer);
+console.log("    ");
+
+console.log(myobj);
+
+
+const myFunction = function() {
+    console.log("Hello World");
+}
