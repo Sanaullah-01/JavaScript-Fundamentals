bbb65945834367c090b0f9c6ff4b529b8c7407e8
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-11 12:28:34 +0000

    Math opr

--- 01-Basics/06-NumMath.js
+++ 01-Basics/06-NumMath.js
@@ -1,3 +1,16 @@
 const  balance = new Number(500)
 
 console.log(balance);
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.random() + 1);
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.7));
+console.log(Math.min(3,4,6,8,1));
+console.log(Math.max(4,6,9,3));
+
+
+
+
