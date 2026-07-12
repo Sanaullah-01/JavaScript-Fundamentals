882e5397d2b1d8fe0e19c3d3000b6721104519a9
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-12 14:11:34 +0000

    nested array to flat by infinity

--- 02-Basics/02-Arrays.js
+++ 02-Basics/02-Arrays.js
@@ -13,7 +13,10 @@ const pkhero = ["RazaSomo", "fazal"]
 // const joined = marvelHeroes.concat(pkhero)
 // console.log(joined);
 
-const allarr = [...marvelHeroes, ...pkhero]
-console.log(allarr);
+// const allarr = [...marvelHeroes, ...pkhero]
+// console.log(allarr);
 
+const arr1  = [1,3,4,6,7,[5,8,90], 5, 7, [6,8,[4,5,6,[3,2,1]]]]
 
+const arr2 = arr1.flat(Infinity)
+console.log(arr2);
