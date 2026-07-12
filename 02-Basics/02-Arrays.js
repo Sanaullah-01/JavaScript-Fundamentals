fc8c6c8c903e067bd2aafe3c4c1356dc7e8591e5
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-12 14:08:27 +0000

    Array concatenation

---
+++ 02-Basics/02-Arrays.js
@@ -0,0 +1,19 @@
+const marvelHeroes = ["ironman", "spiderman", "Thorr"]
+
+const pkhero = ["RazaSomo", "fazal"]
+
+// marvelHeroes.push(pkhero)
+// console.log(marvelHeroes);
+
+// const indianhero = ["Akshay khana", "sanjay", "Sidhu"]
+
+// indianhero.push(marvelHeroes)
+// console.log(indianhero);
+
+// const joined = marvelHeroes.concat(pkhero)
+// console.log(joined);
+
+const allarr = [...marvelHeroes, ...pkhero]
+console.log(allarr);
+
+
