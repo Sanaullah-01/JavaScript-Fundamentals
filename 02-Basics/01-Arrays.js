686129fc372056ccc5492158a968962b98c19670
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-12 12:30:26 +0000

    Array Opers

---
+++ 02-Basics/01-Arrays.js
@@ -0,0 +1,44 @@
+const myArr = [0,1,2,3,4,5,6]
+const myArr2 = [7,8,9,10]
+
+// const myArr3 = myArr.join()
+// console.log(myArr);
+// console.log(myArr3);
+// console.log(typeof myArr3);
+
+console.log('A', myArr)
+
+const myn1 = myArr.slice(1,4)
+console.log(myn1);
+
+console.log('B', myArr);
+const myn2 = myArr.splice(2,3)
+
+console.log('C', myArr);
+console.log(myn2);
+
+
+
+
+
+const myhero = new Array("Alam", "Shetti", "Sagar")
+// console.log(myArr[0]);
+// myArr.push(7)
+ 
+// console.log(myArr);
+// myArr.pop()
+ 
+// console.log(myArr);
+ 
+// console.log(myArr.length);
+
+// myArr.unshift(8)
+// console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+
+
+
+// console.log(myArr.toReversed());
+
+// console.log(typeof(myhero));
