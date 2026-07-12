376c7bc5350925b46afe67be9391eccba62ccc92
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-12 11:45:24 +0000

    Dates in JS

---
+++ 01-Basics/07-date.js
@@ -0,0 +1,10 @@
+let mydate = new Date()
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+
+console.log(mydate.toString().length);
+console.log(mydate.getDate());
+console.log(mydate.getMonth());
+console.log(mydate.getFullYear());
+
