20e835942c8652dd4c9a111c15763aec200556ae
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-13 17:35:54 +0000

    Obj.assign opr

--- 02-Basics/04-obj.js
+++ 02-Basics/04-obj.js
@@ -8,3 +8,10 @@ myobj.age = "23 Y",
 
 console.log(myobj);
 
+
+const ob1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'c', 4:'d'}
+
+// const obj3 = {ob1, obj2}
+const obj3 = Object.assign(ob1, obj2)
+console.log(obj3);
