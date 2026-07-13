a372a1b6095bb6bf63210331206e3ce77cb9db3f
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-13 17:46:01 +0000

    obj campare

--- 02-Basics/04-obj.js
+++ 02-Basics/04-obj.js
@@ -4,14 +4,18 @@ const myobj = {}
 
 myobj.name = "sana ullah",
 myobj.id = 123 ,
-myobj.age = "23 Y", 
+myobj.age = "23 Y"
 
-console.log(myobj);
+// console.log(myobj);
 
 
 const ob1 = {1: 'a', 2: 'b'}
 const obj2 = {3: 'c', 4:'d'}
 
 // const obj3 = {ob1, obj2}
-const obj3 = Object.assign(ob1, obj2)
+const obj3 = Object.assign({}, ob1, obj2);
+// console.log(ob1);
+// console.log(obj2);
 console.log(obj3);
+// console.log(obj3 === ob1);
+// console.log(obj3 === obj2);
