aa0fe0975d3d00cfa3c4d72c35c6d6e67b16d718
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-14 18:21:02 +0000

    refactor object logging and structure in 04-obj.js

--- 02-Basics/04-obj.js
+++ 02-Basics/04-obj.js
@@ -16,6 +16,19 @@ const obj2 = {3: 'c', 4:'d'}
 const obj3 = Object.assign({}, ob1, obj2);
 // console.log(ob1);
 // console.log(obj2);
-console.log(obj3);
+// console.log(obj3);
 // console.log(obj3 === ob1);
 // console.log(obj3 === obj2);
+
+
+const course = {
+    name: "JavaScript",
+    duration: "12 weeks",
+    instructor: "John Doe"
+}
+
+const { name, duration, instructor } = course;
+console.log(course);
+console.log(name, duration, instructor);
+// console.log(duration);
+// console.log(instructor);
