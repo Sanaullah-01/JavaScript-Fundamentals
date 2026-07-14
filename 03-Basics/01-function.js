d7f13d22ac4c0bbe2c02b206bd25c4c6de6143d7
Sana Ullah <sanaullahharaj0435@gmail.com>
2026-07-14 18:53:25 +0000

    refactor: comment out console logs and enhance login user message function

--- 03-Basics/01-function.js
+++ 03-Basics/01-function.js
@@ -3,18 +3,31 @@ function saymyname(){
 
 }
 
-saymyname();
+// saymyname();
 
 function add(a, b){
     return a + b;
 }
 
 const result = add(2, 3);
-console.log(result);
+// console.log(result);
 
 function addmultiply(a,b){
     return [a+b, a*b];
 }
 
 const [sum, multiply] = addmultiply (2, 3);
-console.log("Sum:", sum, "Multiply:", multiply);
+// console.log("Sum:", sum, "Multiply:", multiply);
+
+function loginusermessage (username){
+    if (username === undefined || username === ""){
+        return "Please enter a valid username."
+    }
+    return `${username} has just logged in to our website.`
+}
+
+// (console.log(loginusermessage("sana ullah")))
+
+// (console.log(loginusermessage(" ")))
+
+(console.log(loginusermessage()))
